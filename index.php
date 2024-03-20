87a57237d79631516db396092123fee512667270
fzeeshan <faisalzeeshan2012@gmail.com>
2024-03-20 18:00:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 }
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+#echo $cow->say($text);
 
 header('Content-type: application/json');
 header("Access-Control-Allow-Origin: *");
