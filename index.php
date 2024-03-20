38cca8f0b9f637807a734ccbe6105fe0ecbe52c1
fzeeshan <faisalzeeshan2012@gmail.com>
2024-03-20 18:00:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-
+/*
 use Cowsayphp\Farm;
 
-header('Content-Type: text/plain');
+#header('Content-Type: text/plain');
 
 $text = "Set a message by adding ?message=<message here> to the URL";
 if(isset($_GET['message']) && $_GET['message'] != '') {
@@ -11,6 +11,7 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 }
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
+*/
 #echo $cow->say($text);
 
 header('Content-type: application/json');
